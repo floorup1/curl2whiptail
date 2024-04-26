b3582926470173e1cf6cae5a1a44fb64276c8507
floorup1 <floorup@outlook.com>
2024-04-26 15:35:05 +0400

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -3,20 +3,32 @@ packages_string=$(apt list --upgradable 2>&1 | sed -r 's/\/.+//' | sed 's/.*\.//
 echo $packages_string
 echo
 packages_array=($(echo $packages_string | tr ' ' '\n'))
-for i in "${packages_array[@]}"
+declare -a msgs=${packages_array[@]}
+for i in "${msgs[@]}"
 do
     echo $i
 done
 n=${#packages_array[@]}
-j=0
-while [ "$j" -lt "$n" ]; do
-    pct=$(( j * 100 / n ))
-    echo XXX
-    echo $j
-    echo "${msgs[j]}"
-    echo XXX
-    echo "$pct"
-    eval "${commands[j]}"
-    j=$((j + 1))
-    sleep 1s
-done | whiptail --title "Gauge" --gauge "Please wait..." 10 60 0
+#echo
+#msgs[@]="Устанавливается "${packages_array[@]}
+#echo ${msgs[@]}
+#for (( k=0; k < n; k++ ))
+#do
+#msgs[k]="Устанавливается "${packages_array[k]}
+#echo ${msgs[k]}
+#{commands[k]}="apt install -y "${packages_array[k]}
+#echo ${commands[k]}
+#done
+
+#j=0
+#while [ "$j" -lt "$n" ]; do
+#    pct=$(( j * 100 / n ))
+#    echo XXX
+#    echo $j
+#    echo "${msgs[j]}"
+#    echo XXX
+#    echo "$pct"
+#    eval "${commands[j]}"
+#    j=$((j + 1))
+#    sleep 1s
+#done | whiptail --title "Gauge" --gauge "Please wait..." 10 60 0
